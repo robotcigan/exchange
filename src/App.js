3503d901566d28bdbd2a7dc8c029c6587b262a17
Arseny Ivanov <arsenyivanov@MacBook-Pro-2.local>
2023-02-24 23:22:44 +0300

    new functional

--- src/App.js
+++ src/App.js
@@ -1,52 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';  
+import React from 'react';
+// import Container from 'react-bootstrap/Container';
 import ExchangeForm from './components/form'
 import Rates from './rates.json'
 
-// let testArr = [
-// ]
+import './styles/main.sass' 
 
 function App() {
-  // console.log('rates', Rates[0])
-  // console.log(Rates[0].rates.filter(rate => rate.currency === "USD"))
+  // console.log(Rates[0].rates.filter(rate => {
+  //   let currency = rate.currency === "USD"
+  //   return currency
+  // }))
+
   return (
     <div className="App">
-      <Container>
+      <div className='wrapper'>
         <h4>openexchange.com</h4>
         <ExchangeForm />
 
-        <Table>
-          <thead>
-            <tr>
-              <th>Банк</th>
-              <th>Покупает $</th>
-              <th>Продает $</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Rates.map((el, index) =>
-              // тут нужно еще вытащить buy, sell
-              <p key={index}>{el.rates.filter(rate => rate.currency === "USD")}</p>
-            )}
-            {/* {Rates.map((el, index) =>
-              <tr key={index}>
-                <td>{el.bank}</td>
-                {el.rates.map((rate, index) =>
-                  <>
-                    <td class="text-success">
-                      {rate.buy}
-                    </td>
-                    <td class="text-danger">
-                      {rate.sell}
-                    </td>
-                  </>
-                )}
-              </tr>
-            )} */}
-          </tbody>
-        </Table>
-      </Container>
+        <thead>
+          <tr>
+            <th>Банк</th>
+            <th>Покупает $</th>
+            <th>Продает $</th>
+          </tr>
+        </thead>
 
+        {Rates.map((el, index) => {
+          let currency = el.rates.filter(rate => rate.currency === "USD")
+          return(
+            <React.Fragment key={index}>
+              <p>{currency[0].buy}</p>
+              <p>{currency[0].sell}</p>
+            </React.Fragment>
+          )
+        })}
+
+      </div>
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,28 @@
-import Form from 'react-bootstrap/Form';  
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
 
-export default function Test() {
+export default function ExchangeForm() {
+  // const [count, setCount] = useState(0)
+  const [currencyFrom, setCurrencyFrom] = useState(100)
+  const [currencyTo, setCurrencyTo] = useState(94.42)
+  function handleChangeCurrencyFrom(event) {
+    setCurrencyFrom(event.target.value)
+    setCurrencyTo(event.target.value * 0.95)
+  }
+  function handleChangeCurrencyTo(event) {
+    setCurrencyTo(event.target.value)
+    setCurrencyFrom(event.target.value * 1.06)
+  }
   return (
     <Form>
       <Form.Group className="mb-3">
         <Form.Label>$</Form.Label>
-        <Form.Control type="number" value="100" />
+        <Form.Control type="number" value={currencyFrom} name="currencyFrom" onChange={handleChangeCurrencyFrom} />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>€</Form.Label>
-        <Form.Control type="number" value="94.42" />
+        <Form.Control type="number" value={currencyTo} name="currencyTo" onChange={handleChangeCurrencyTo} />
       </Form.Group>
     </Form>
   );
